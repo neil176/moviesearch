d568e2694531cc23871a57c97469c1e1c1b28737
Neil Steiner <npsteiner@gmail.com>
2019-10-14 08:11:42 -0400

    Remove outdated comments from handlers

--- server/src/handlers.js
+++ server/src/handlers.js
@@ -2,110 +2,10 @@ const { tmdbRp } = require('./tmdbUtils.js');
 
 /*
   Possible additions:
-  - add a query formatting method for better results 
+  - add a query formatting method for better results
   - add a formatting layer before returnin the body to remove unnecessary data
 */
 
-
-/* sample response from tmdb:
-{
-  "images": {
-    "base_url": "http://image.tmdb.org/t/p/",
-    "secure_base_url": "https://image.tmdb.org/t/p/",
-    "backdrop_sizes": [
-      "w300",
-      "w780",
-      "w1280",
-      "original"
-    ],
-    "logo_sizes": [
-      "w45",
-      "w92",
-      "w154",
-      "w185",
-      "w300",
-      "w500",
-      "original"
-    ],
-    "poster_sizes": [
-      "w92",
-      "w154",
-      "w185",
-      "w342",
-      "w500",
-      "w780",
-      "original"
-    ],
-    "profile_sizes": [
-      "w45",
-      "w185",
-      "h632",
-      "original"
-    ],
-    "still_sizes": [
-      "w92",
-      "w185",
-      "w300",
-      "original"
-    ]
-  },
-  "change_keys": [
-    "adult",
-    "air_date",
-    "also_known_as",
-    "alternative_titles",
-    "biography",
-    "birthday",
-    "budget",
-    "cast",
-    "certifications",
-    "character_names",
-    "created_by",
-    "crew",
-    "deathday",
-    "episode",
-    "episode_number",
-    "episode_run_time",
-    "freebase_id",
-    "freebase_mid",
-    "general",
-    "genres",
-    "guest_stars",
-    "homepage",
-    "images",
-    "imdb_id",
-    "languages",
-    "name",
-    "network",
-    "origin_country",
-    "original_name",
-    "original_title",
-    "overview",
-    "parts",
-    "place_of_birth",
-    "plot_keywords",
-    "production_code",
-    "production_companies",
-    "production_countries",
-    "releases",
-    "revenue",
-    "runtime",
-    "season",
-    "season_number",
-    "season_regular",
-    "spoken_languages",
-    "status",
-    "tagline",
-    "title",
-    "translations",
-    "tvdb_id",
-    "tvrage_id",
-    "type",
-    "video",
-    "videos"
-  ]
-}
-*/
 
 function formatConfig(configBody) {
   const {
@@ -175,27 +75,6 @@ async function getPopular(req, res) {
   return res.send(body);
 }
 
-// async function getDetail(req, res) {
-//   const {
-//     params: {
-//       movieId,
-//     } = {},
-//   } = req;
-
-//   console.log(req.params);
-
-//   let body;
-//   try {
-//     body = await tmdbRp({
-//       url: `/movie/${ movieId }`,
-//     });
-//   }
-//   catch (e) {
-//     console.error('[getDetail]', e);
-//   }
-//   if (!body) return res.sendStatus(502);
-//   return res.send(body);
-// }
 
 async function getDetail(req, res) {
   const {
@@ -203,8 +82,6 @@ async function getDetail(req, res) {
       movieId,
     } = {},
   } = req;
-
-  console.log(req.params);
 
   let body;
   try {
